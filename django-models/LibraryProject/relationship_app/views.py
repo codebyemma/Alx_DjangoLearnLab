4c0016b0413171130aba795ce2ae843e8fa54c30
codebyemma <mysubbacc@gmail.com>
2025-11-09 21:26:49 +0100

    jhjhfd

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library, Book
+from django.contrib.auth import login
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
