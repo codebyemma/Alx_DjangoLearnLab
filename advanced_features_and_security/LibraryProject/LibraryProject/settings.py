8f6865006425a1f09d5953dc8030f09d8406f399
codebyemma <mysubbacc@gmail.com>
2025-11-17 00:04:10 +0100

    add

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
     }
 }
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # Password validation
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    """Admin settings for CustomUser model."""
+
+    model = CustomUser
+
+    # Fields visible when editing a user
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    # Fields shown when creating a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+    search_fields = ("username", "email")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
