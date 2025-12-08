72d4bf62a3d026a817c3f0ad470cce71389e6e5c
codebyemma <mysubbacc@gmail.com>
2025-12-08 20:35:35 +0100

    jjdjs

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookListCreateAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -10,3 +11,4 @@ class BookListCreateAPIView(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
