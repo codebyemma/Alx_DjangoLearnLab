4947477cf18df5d4c6affbf167a25a8f5afcb3ca
codebyemma <mysubbacc@gmail.com>
2025-12-13 13:42:47 +0100

    srfs

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e1rdf!z%&c+5%vm63bfc*40v9hlggj!h#au6xir9dop%jfw^6_'
+SECRET_KEY = 'django-insecure-3v@ox&g93!u=+0h$mcli*os)xs9yj*!((jy+a+o=%$u7@4!x3i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party
     'rest_framework',
+
+    # Local
     'api',
 ]
 
