6a82515eb70a3a1828fd47f7f2e01b82d214cca7
codebyemma <mysubbacc@gmail.com>
2025-12-13 14:25:33 +0100

    srfs

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        """
+        Optionally filter books by publication year:
+        /api/books/?year=2020
+        """
+        queryset = Book.objects.all()
+        year = self.request.query_params.get('year')
+        if year:
+            queryset = queryset.filter(publication_year=year)
+        return queryset
+
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    Handles retrieval of a single Book instance by ID.
+    Accessible to all users.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    Handles creation of a new Book.
+    Restricted to authenticated users only.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    Handles updating an existing Book.
+    Restricted to authenticated users only.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    Handles deletion of a Book instance.
+    Restricted to authenticated users only.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
