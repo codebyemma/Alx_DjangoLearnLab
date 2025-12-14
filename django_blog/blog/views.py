85a7746a3e62c539be0f026e2746c29b1462834e
codebyemma <mysubbacc@gmail.com>
2025-12-14 19:01:13 +0100

    fd

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -129,4 +129,16 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
-    
+def search(request):
+    query = request.GET.get('q', '')
+    results = Post.objects.filter(
+        Q(title__icontains=query) |
+        Q(content__icontains=query) |
+        Q(tags__name__icontains=query)
+    ).distinct()
+
+    return render(
+        request,
+        'blog/search_results.html',
+        {'posts': results, 'query': query}
+    )
